e3d9aa8e967317b58cca7248c02d5f675c2b4b85
LonelyBald <rumba2922@gmail.com>
2022-05-25 00:18:13 +0300

    init

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories () {
+    return (
+        <div className="categories">
+              <ul>
+                <li className="active">ВАНЯ ПИДОР</li>
+                <li>ВАНЯ ПИДОР</li>
+                <li>ВАНЯ ПИДОР</li>
+                <li>ВАНЯ ПИДОР</li>
+                <li>ВАНЯ ПИДОР</li>
+                <li>ВАНЯ ПИДОР</li>
+              </ul>
+            </div>
+    )
+}
+
+export default Categories
