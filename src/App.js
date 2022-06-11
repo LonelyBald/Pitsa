e7533112fee8db09a48caf79cc09bdcfaab2787d
LonelyBald <rumba2922@gmail.com>
2022-06-11 20:06:27 +0300

    Add skeleton and routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './scss/app.scss';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
 
 function App() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    fetch('https://629de05cc6ef9335c0a8e475.mockapi.io/api/pitsa/items')
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все питсы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock {...obj} key={obj.title + obj.price} />
-            ))}
-          </div>
-        </div>
-      </div>
+      <Outlet />
     </div>
   );
 }
